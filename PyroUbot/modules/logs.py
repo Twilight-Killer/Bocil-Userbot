828c233c84bb170f548ae0fd2612b052af5ab46b
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-10-12 21:34:13 +0700

    Update logs.py

--- PyroUbot/modules/logs.py
+++ PyroUbot/modules/logs.py
@@ -75,6 +75,6 @@ async def create_logs(client):
     logs = await client.create_channel(f"Logs Ubot: {bot.me.username}")
     await client.set_chat_photo(
         logs.id,
-        video="storage/logs.mp4",
+        video="storage/Bocil_Logs.mp4",
     )
     return logs.id
