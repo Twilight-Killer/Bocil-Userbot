0ca5ce978e29ddf8aec6fbcd1b1734e666424106
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-12-05 20:56:20 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -52,6 +52,7 @@ class Ubot(Client):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs, device_model="BuruTaniUbot")
+        self.group_call = GroupCallFactory(self).get_file_group_call("input.raw")
 
     def on_message(self, filters=None, group=-1):
         def decorator(func):
