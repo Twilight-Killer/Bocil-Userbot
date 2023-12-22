b66904669e1abb84aff7e2abd2d9a2f95ee66c52
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-12-22 21:32:30 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -18,14 +18,22 @@ from PyroUbot.config import *
 
 class ConnectionHandler(logging.Handler):
     def emit(self, record):
-        if "TimeoutError" in record.getMessage():
-            os.system(f"kill -9 {os.getpid()} && python3 -m PyroUbot")
+        for error_type in ["OSError", "AUTH_KEY_UNREGISTERED"]:
+            if error_type in record.getMessage():
+                os.system(f"kill -9 {os.getpid()} && python3 -m PyroUbot")
 
-logging.basicConfig(
-    format="[%(levelname)s] - %(name)s - %(message)s",
-    level=logging.ERROR,
-    handlers=[logging.StreamHandler(), ConnectionHandler()],
-)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+formatter = logging.Formatter("[%(levelname)s] - %(name)s - %(message)s", "%m-%d %H:%M")
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+connection_handler = ConnectionHandler()
+logger.addHandler(stream_handler)
+logger.addHandler(connection_handler)
 
 
 class Bot(Client):
