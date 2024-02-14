5c98c543c5991d5ab8c86762efea4cbeaba8a87e
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-14 07:51:54 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -1,9 +1,7 @@
-import uvloop
-uvloop.install()
-
 import logging
 import os
 import re
+import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.enums import ParseMode
@@ -16,25 +14,66 @@ from pytgcalls import GroupCallFactory
 from PyroUbot.config import *
 
 
+class ConnectionError(Exception):
+    pass
+
 class ConnectionHandler(logging.Handler):
     def emit(self, record):
-        for error_type in ["OSError", "AUTH_KEY_UNREGISTERED"]:
+        for error_type in ["OSErro", "TimeoutError"]:
             if error_type in record.getMessage():
-                os.system(f"kill -9 {os.getpid()} && python3 -m PyroUbot")
+                self.handle_error(record.getMessage())
 
+    def handle_error(self, error_message):
+        self.log_error(error_message)
+        raise ConnectionError(error_message)
+
+    def log_error(self, error_message):
+        with open("error_log.txt", "a") as log_file:
+            log_file.write(f"Error: {error_message}\n")
+
+# Konfigurasi logging
+logging.basicConfig(level=logging.ERROR, format='%(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+handler = ConnectionHandler()
+logger.addHandler(handler)
 
-formatter = logging.Formatter("[%(levelname)s] - %(name)s - %(message)s")
+max_retries = 3
+retries = 0
 
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
+while retries < max_retries:
+    try:
+        # Simulasi koneksi yang gagal (ganti dengan kode sesuai kebutuhan)
+        raise OSError("Koneksi Gagal")
+    except OSError as e:
+        logger.error(f"Terjadi kesalahan: {e}")
+        retries += 1
+        if retries < max_retries:
+            print(f"Mencoba kembali... (percobaan ke-{retries})")
+        else:
+            print("Gagal setelah beberapa percobaan.")
+            break
+    except ConnectionError as ce:
+        logger.error(f"Terjadi kesalahan koneksi: {ce}")
+        retries += 1
+        if retries < max_retries:
+            print(f"Mencoba kembali... (percobaan ke-{retries})")
+        else:
+            print("Gagal setelah beberapa percobaan.")
+            break
 
-connection_handler = ConnectionHandler()
-logger.addHandler(stream_handler)
-logger.addHandler(connection_handler)
-
+# Fungsi untuk melakukan permintaan ke kanal
+async def get_channel_messages(channel):
+    try:
+        # Lakukan permintaan ke kanal
+        messages = await bot.get_messages(channel)
+        return messages
+    except FloodWait as e:
+        # Tangani kesalahan FloodWait dengan menunggu waktu yang diberikan oleh Telegram
+        await asyncio.sleep(e.x)
+        # Coba kembali permintaan setelah menunggu
+        messages = await get_channel_messages(channel)
+        return messages
 
 class Bot(Client):
     def __init__(self, **kwargs):
