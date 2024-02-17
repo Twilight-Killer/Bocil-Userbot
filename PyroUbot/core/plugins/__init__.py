1cef96567d9ce38dfe3cc8a3c7adf0d74bf3e9aa
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-17 22:30:04 +0700

    Update __init__.py

--- PyroUbot/core/plugins/__init__.py
+++ PyroUbot/core/plugins/__init__.py
@@ -3,11 +3,16 @@ from os.path import basename, dirname, isfile
 
 
 def loadP():
-    mod_paths = glob(f"{dirname(file)}/*.py")
+    mod_paths = glob(f"{dirname(__file__)}/*.py")
     return sorted(
         [
             basename(f)[:-3]
             for f in mod_paths
-            if isfile(f) and f.endswith(".py") and not f.endswith("init.py")
+            if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
         ]
     )
+
+
+for module_name in loadP():
+    import_statement = f"from PyroUbot.core.plugins.{module_name} import *"
+    exec(import_statement)
