79d6cda9a512f71cc02f49691bf4ec5e92aa08dd
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-19 15:33:25 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -129,6 +129,7 @@ class Ubot(Client):
     def __init__(self, **kwargs):
         super().__init__(**kwargs, device_model="V1PremUbot")
         self.group_call = GroupCallFactory(self).get_file_group_call("input.raw")
+        self.start_time = datetime.now()
     def on_message(self, filters=None, group=-1):
         def decorator(func):
             for ub in self._ubot:
