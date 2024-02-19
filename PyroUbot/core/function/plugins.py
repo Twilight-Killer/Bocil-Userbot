3c8ead4819022670bd513bad53220a84adc50f26
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-19 15:04:27 +0700

    Update plugins.py

--- PyroUbot/core/function/plugins.py
+++ PyroUbot/core/function/plugins.py
@@ -15,13 +15,16 @@ HELP_COMMANDS = {}
 async def loadPlugins():
     modules = loadModule()
     for mod in modules:
-        imported_module = import_module(f"PyroUbot.modules.{mod}")
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELP_COMMANDS[
-                    imported_module.__MODULE__.replace(" ", "_").lower()
-                ] = imported_module
+        if mod == "stats":
+            import_module(f"PyroUbot.modules.{mod}")
+        else:
+            imported_module = import_module(f"PyroUbot.modules.{mod}")
+            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+                imported_module.__MODULE__ = imported_module.__MODULE__
+                if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                    HELP_COMMANDS[
+                        imported_module.__MODULE__.replace(" ", "_").lower()
+                    ] = imported_module
     print(f"[🤖 @{bot.me.username} 🤖] [🔥 TELAH BERHASIL DIAKTIFKAN! 🔥]")
     TM = await bot.send_message(
         OWNER_ID,
