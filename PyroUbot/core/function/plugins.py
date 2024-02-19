1eccb768b958c9695e46df7a3ef8825317ee4aea
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-19 14:44:49 +0700

    Update plugins.py

--- PyroUbot/core/function/plugins.py
+++ PyroUbot/core/function/plugins.py
@@ -16,14 +16,12 @@ async def loadPlugins():
     modules = loadModule()
     for mod in modules:
         imported_module = import_module(f"PyroUbot.modules.{mod}")
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELP_COMMANDS[
-                    imported_module.__MODULE__.replace(" ", "_").lower()
-                ] = imported_module
+        if hasattr(imported_module, "__MODULE__") and isinstance(imported_module.__MODULE__, str):
+            HELP_COMMANDS[imported_module.__MODULE__.replace(" ", "_").lower()] = imported_module
     print(f"[🤖 @{bot.me.username} 🤖] [🔥 TELAH BERHASIL DIAKTIFKAN! 🔥]")
-    TM = await bot.send_message(
+
+async def sendStartupMessage():
+    await bot.send_message(
         OWNER_ID,
         f"""
 <b>🤖 {bot.me.mention} ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋᴛɪꜰᴋᴀɴ</b>
@@ -40,7 +38,6 @@ async def loadPlugins():
             ]
         ),
     )
-
 
 @PY.CALLBACK("0_cls")
 async def _(client, callback_query):
