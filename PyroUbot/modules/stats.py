b39c4e2636f3230a22f2dec7e6aa42478efd6417
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-19 18:28:49 +0700

    Update stats.py

--- PyroUbot/modules/stats.py
+++ PyroUbot/modules/stats.py
@@ -64,7 +64,4 @@ async def stats_command(client, message):
     await message.reply(
         text=stats_message,
         reply_markup=inline_keyboard
-    )
     
-    # Reply with stats message
-    await message.reply(stats_message)
