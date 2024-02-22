337afa3adaee0851d35e3be04369ddbb8427e960
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-22 19:23:26 +0700

    Update dbcontrol.py

--- PyroUbot/modules/dbcontrol.py
+++ PyroUbot/modules/dbcontrol.py
@@ -7,8 +7,8 @@ async def _(client, message):
     await prem_user(client, message)
 
 
-@PY.BOT("unprem", FILTERS.OWNER, SUDO=False)
-@PY.UBOT("unprem", FILTERS.ME_OWNER)
+@PY.BOT("unprem", FILTERS.OWNER)
+@PY.UBOT("unprem", FILTERS.ME_OWNER, SUDO=False)
 async def _(client, message):
     await unprem_user(client, message)
 
