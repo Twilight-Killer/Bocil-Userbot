91a4c627868f80759b76fffada0e3c52199ea16b
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-22 19:50:18 +0700

    Update dbcontrol.py

--- PyroUbot/modules/dbcontrol.py
+++ PyroUbot/modules/dbcontrol.py
@@ -14,43 +14,43 @@ async def _(client, message):
 
 
 @PY.BOT("getprem", FILTERS.OWNER)
-@PY.UBOT("getprem", FILTERS.ME_OWNER, SUDO=True)
+@PY.UBOT("getprem", FILTERS.ME_OWNER
 async def _(cliebt, message):
     await get_prem_user(client, message)
 
 
 @PY.BOT("seles", FILTERS.OWNER)
-@PY.UBOT("seles", FILTERS.ME_OWNER, SUDO=False)
+@PY.UBOT("seles", FILTERS.ME_OWNER)
 async def _(client, message):
     await seles_user(client, message)
 
 
 @PY.BOT("unseles", FILTERS.OWNER)
-@PY.UBOT("unseles", FILTERS.ME_OWNER, SUDO=False)
+@PY.UBOT("unseles", FILTERS.ME_OWNER)
 async def _(client, message):
     await unseles_user(client, message)
 
 
 @PY.BOT("getseles", FILTERS.OWNER)
-@PY.UBOT("getseles", FILTERS.ME_OWNER, SUDO=False)
+@PY.UBOT("getseles", FILTERS.ME_OWNER)
 async def _(client, message):
     await get_seles_user(client, message)
 
 
 @PY.BOT("time", FILTERS.OWNER)
-@PY.UBOT("time", FILTERS.ME_OWNER, SUDO=False)
+@PY.UBOT("time", FILTERS.ME_OWNER)
 async def _(client, message):
     await expired_add(client, message)
 
 
 @PY.BOT("cek", FILTERS.OWNER)
-@PY.UBOT("cek", FILTERS.ME_OWNER, SUDO=False)
+@PY.UBOT("cek", FILTERS.ME_OWNER)
 async def _(client, message):
     await expired_cek(client, message)
 
 
 @PY.BOT("untime", FILTERS.OWNER)
-@PY.UBOT("untime", FILTERS.ME_OWNER, SUDO=False)
+@PY.UBOT("untime", FILTERS.ME_OWNER)
 async def _(client, message):
     await un_expired(client, message)
 
