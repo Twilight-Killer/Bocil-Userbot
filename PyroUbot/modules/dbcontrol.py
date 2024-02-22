59dd1c7bc65088f475b09602fcf67e1a345491b1
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-22 19:47:21 +0700

    Update dbcontrol.py

--- PyroUbot/modules/dbcontrol.py
+++ PyroUbot/modules/dbcontrol.py
@@ -14,7 +14,7 @@ async def _(client, message):
 
 
 @PY.BOT("getprem", FILTERS.OWNER)
-@PY.UBOT("getprem", FILTERS.ME_OWNER, SUDO=False)
+@PY.UBOT("getprem", FILTERS.ME_OWNER, SUDO=True)
 async def _(cliebt, message):
     await get_prem_user(client, message)
 
