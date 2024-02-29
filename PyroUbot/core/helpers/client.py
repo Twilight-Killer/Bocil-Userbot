9f98e0126ebe25aeb8b9047b71e6a40d58ad6340
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-29 14:26:09 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -45,27 +45,26 @@ class PY:
         return wrapper
 
     @staticmethod
-    def UBOT(command, filter=FILTERS.ME, SUDO=True):
-        def decorator(func):
-            @ubot.on_message(
-                ubot.cmd_prefix(command) & filter
-                if not SUDO
-                else ubot.cmd_prefix(command)
-            )
-            async def wrapped_func(client, message):
-                user = message.from_user or message.sender_chat
-                is_self = user.is_self if message.from_user else False 
-                sudo_id = await get_list_from_vars(client.me.id, "SUDO_USERS")
+def UBOT(command, filter=FILTERS.ME, SUDO=True):
+    def decorator(func):
+        @ubot.on_message(
+            ubot.cmd_prefix(command) & filter
+            if not SUDO
+            else ubot.cmd_prefix(command) & filters.user(SUDO_USERS)
+        )
+        async def wrapped_func(client, message):
+            user = message.from_user or message.sender_chat
+            is_self = user.is_self if message.from_user else False 
 
-                if SUDO and is_self or user.id in sudo_id:
-                    return await func(client, message)
+            if SUDO and is_self or user.id in SUDO_USERS:
+                return await func(client, message)
 
-                elif not SUDO:
-                    return await func(client, message)
+            elif not SUDO:
+                return await func(client, message)
 
-            return wrapped_func
+        return wrapped_func
 
-        return decorator
+    return decorator
 
     @staticmethod
     def INLINE(command):
