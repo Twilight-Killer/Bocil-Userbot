7c83323d51bb6394b2e680584888de35de6279ed
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-03-18 16:09:22 +0700

    Update userbot.py

--- PyroUbot/core/database/userbot.py
+++ PyroUbot/core/database/userbot.py
@@ -1,35 +1,47 @@
-from PyroUbot.core.database import mongodb
+from motor.motor_asyncio import AsyncIOMotorClient
+from PyroUbot.config import MONGO_URL
+
+mongo_client = AsyncIOMotorClient(MONGO_URL)
+mongodb = mongo_client.pyro_ubot
 
 ubotdb = mongodb.ubot
 
+async def get_user_data():
+    """
+    Get user data from the database.
+    """
+    data = []
+    async for ubot in ubotdb.find({"user_id": {"$exists": 1}}):
+        data.append({
+            "name": str(ubot["user_id"]),
+            "api_id": ubot["api_id"],
+            "api_hash": ubot["api_hash"],
+            "session_string": ubot["session_string"]
+        })
+    return data
 
+# Menggunakan async def untuk fungsi-fungsi yang melakukan operasi yang membutuhkan waktu
 async def add_ubot(user_id, api_id, api_hash, session_string):
-    return await ubotdb.update_one(
+    await ubotdb.update_one(
         {"user_id": user_id},
-        {
-            "$set": {
-                "api_id": api_id,
-                "api_hash": api_hash,
-                "session_string": session_string,
-            }
-        },
+        {"$set": {"api_id": api_id, "api_hash": api_hash, "session_string": session_string}},
         upsert=True,
     )
 
-
+# Menggunakan async def untuk fungsi-fungsi yang melakukan operasi yang membutuhkan waktu
 async def remove_ubot(user_id):
-    return await ubotdb.delete_one({"user_id": user_id})
+    await ubotdb.delete_one({"user_id": user_id})
 
-
+# Menggunakan async def untuk fungsi-fungsi yang melakukan operasi yang membutuhkan waktu
 async def get_userbots():
     data = []
     async for ubot in ubotdb.find({"user_id": {"$exists": 1}}):
         data.append(
-            dict(
-                name=str(ubot["user_id"]),
-                api_id=ubot["api_id"],
-                api_hash=ubot["api_hash"],
-                session_string=ubot["session_string"],
-            )
+            {
+                "name": str(ubot["user_id"]),
+                "api_id": ubot["api_id"],
+                "api_hash": ubot["api_hash"],
+                "session_string": ubot["session_string"],
+            }
         )
     return data
