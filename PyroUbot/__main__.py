c71ba49f6f50b249cbf8dbab2bab7cb8831b091e
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-03-18 22:31:57 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -1,45 +1,47 @@
 import asyncio
-
 from pyrogram import idle
 
 from PyroUbot import *
-
 
 async def start_ubot(user_id, _ubot):
     ubot_ = Ubot(**_ubot)
     try:
         await asyncio.wait_for(ubot_.start(), timeout=30)
     except asyncio.TimeoutError:
-        await remove_ubot(user_id)
-        await add_prem(user_id)
-        await rm_all(user_id)
-        await rem_pref(user_id)
-        await remove_all_vars(user_id)
-        await rem_uptime(user_id)
-        for X in await get_chat(user_id):
-            await remove_chat(user_id, X)
-        await sending_user(user_id)
-        print(f"[𝗜𝗡𝗙𝗢] - ({user_id}) 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
-    except:
-        await remove_ubot(user_id)
-        await rm_all(user_id)
-        await remove_all_vars(user_id)
-        await rem_pref(user_id)
-        await rem_uptime(user_id)
-        await rem_expired_date(user_id)
-        for X in await get_chat(user_id):
-            await remove_chat(user_id, X)
-        print(f"✅ {user_id} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦")
+        await handle_timeout(user_id)
+    except Exception as e:
+        await handle_error(user_id, e)
 
+async def handle_timeout(user_id):
+    await remove_ubot(user_id)
+    await add_prem(user_id)
+    await rm_all(user_id)
+    await rem_pref(user_id)
+    await remove_all_vars(user_id)
+    await rem_uptime(user_id)
+    for X in await get_chat(user_id):
+        await remove_chat(user_id, X)
+    await sending_user(user_id)
+    print(f"[INFO] - ({user_id})  𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
+
+async def handle_error(user_id, error):
+    await remove_ubot(user_id)
+    await rm_all(user_id)
+    await remove_all_vars(user_id)
+    await rem_pref(user_id)
+    await rem_uptime(user_id)
+    await rem_expired_date(user_id)
+    for X in await get_chat(user_id):
+        await remove_chat(user_id, X)
+    print(f"✅ {user_id} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦: {error}")
 
 async def main():
     tasks = [
-        asyncio.create_task(start_ubot(int(_ubot["name"]), _ubot))
+        start_ubot(int(_ubot["name"]), _ubot)
         for _ubot in await get_userbots()
     ]
     await asyncio.gather(*tasks, bot.start())
     await asyncio.gather(loadPlugins(), installPeer(), expiredUserbots(), idle())
-
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop_policy().get_event_loop()
