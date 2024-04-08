ffe6d7a9399980759eab221910b862cb1276f87c
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-08 13:46:09 +0700

    Update saved.py

--- PyroUbot/core/database/saved.py
+++ PyroUbot/core/database/saved.py
@@ -2,23 +2,24 @@ from PyroUbot.core.database import mongodb
 
 chatsdb = mongodb.chats
 
-
 async def get_chat(user_id):
     chat = await chatsdb.find_one({"chat": user_id})
     if not chat:
         return []
-    return chat["list"]
-
+    return chat.get("list", [])
 
 async def add_chat(user_id, chat_id):
-    list = await get_chat(user_id)
-    list.append(chat_id)
-    await chatsdb.update_one({"chat": user_id}, {"$set": {"list": list}}, upsert=True)
-    return True
-
+    chat_list = await get_chat(user_id)
+    if chat_id not in chat_list:
+        chat_list.append(chat_id)
+        await chatsdb.update_one({"chat": user_id}, {"$set": {"list": chat_list}}, upsert=True)
+        return True
+    return False
 
 async def remove_chat(user_id, chat_id):
-    list = await get_chat(user_id)
-    list.remove(chat_id)
-    await chatsdb.update_one({"chat": user_id}, {"$set": {"list": list}}, upsert=True)
-    return True
+    chat_list = await get_chat(user_id)
+    if chat_id in chat_list:
+        chat_list.remove(chat_id)
+        await chatsdb.update_one({"chat": user_id}, {"$set": {"list": chat_list}}, upsert=True)
+        return True
+    return False
