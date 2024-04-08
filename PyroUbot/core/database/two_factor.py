b5e33eeab642c9840eb053876a3ac38f48694498
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-08 13:44:09 +0700

    Update two_factor.py

--- PyroUbot/core/database/two_factor.py
+++ PyroUbot/core/database/two_factor.py
@@ -1,23 +1,21 @@
 from PyroUbot.core.database import mongo_client
 
-factor = mongo_client["PyroUbot"]["factor"]
+factor_collection = mongo_client["PyroUbot"].get_collection("factor")
 
 
 async def get_two_factor(user_id):
-    user = await factor.users.find_one({"_id": user_id})
+    user = await factor_collection.find_one({"_id": user_id})
     if user:
         return user.get("expire_date")
     else:
         return None
 
-
 async def set_two_factor(user_id, expire_date):
-    await factor.users.update_one(
+    await factor_collection.update_one(
         {"_id": user_id}, {"$set": {"expire_date": expire_date}}, upsert=True
     )
 
-
 async def rem_two_factor(user_id):
-    await factor.users.update_one(
+    await factor_collection.update_one(
         {"_id": user_id}, {"$unset": {"expire_date": ""}}, upsert=True
     )
