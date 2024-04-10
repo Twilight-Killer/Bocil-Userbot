6733c8cbfe73bfafe86ffd99c333740ac873b93b
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-11 05:47:43 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -3,8 +3,10 @@ import os
 import re
 import asyncio
 import uvloop
+uvloop.install()
 
 from pyrogram import Client, filters
+from pyrogram.enums import ParseMode
 from pyrogram.handlers import CallbackQueryHandler, MessageHandler
 from pyrogram.types import Message
 from pyromod import listen
@@ -12,24 +14,9 @@ from pytgcalls import GroupCallFactory
 
 from PyroUbot.config import *
 
-class ConnectionHandler(logging.Handler):
-    def emit(self, record):
-        for error_type in ["OSErro", "TimeoutError"]:
-            if error_type in record.getMessage():
-                self.handle_error(record.getMessage())
-
-    def handle_error(self, error_message):
-        self.log_error(error_message)
-        raise ConnectionError(error_message)
-
-    def log_error(self, error_message):
-        with open("error_log.txt", "a") as log_file:
-            log_file.write(f"Error: {error_message}\n")
 
 logging.basicConfig(level=logging.ERROR, format='%(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-handler = ConnectionHandler()
-logger.addHandler(handler)
 
 max_retries = 3
 retries = 0
@@ -45,31 +32,12 @@ while retries < max_retries:
         else:
             print("Gagal setelah beberapa percobaan.")
             break
-    except ConnectionError as ce:
-        logger.error(f"Terjadi kesalahan koneksi: {ce}")
-        retries += 1
-        if retries < max_retries:
-            print(f"Mencoba kembali... (percobaan ke-{retries})")
-        else:
-            print("Gagal setelah beberapa percobaan.")
-            break
 
-async def get_channel_messages(channel):
-    try:
-        messages = await bot.get_messages(channel)
-        return messages
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        chat = await bot.get_chat(e.chat_id)
-        chat_name = chat.title if chat.title else chat.first_name
-        print(f"FloodWait: {chat_name} harus menunggu {e.x} detik sebelum mengirim pesan lagi.")
-        await bot.send_message(e.chat_id, f"Maaf, {chat_name}, Anda harus menunggu {e.x} detik sebelum mengirim pesan lagi.")
-        return await get_channel_messages(channel)
 
 class Bot(Client):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.device_model = "BuruTaniBot"
+        super().__init__(**kwargs) 
+        self.device_model="BuruTaniUbot"
 
     def on_message(self, filters=None, group=-1):
         def decorator(func):
@@ -86,15 +54,16 @@ class Bot(Client):
     async def start(self):
         await super().start()
 
+
 class Ubot(Client):
-    _get_my_id = []
     _ubot = []
     _prefix = {}
+    _get_my_id = []
     _translate = {}
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        device_model="BuruTaniUbot"
+        super().__init__(**kwargs) 
+        self.device_model="BuruTaniUbot"
         self.group_call = GroupCallFactory(self).get_file_group_call("input.raw")
 
     def on_message(self, filters=None, group=-1):
@@ -102,7 +71,6 @@ class Ubot(Client):
             for ub in self._ubot:
                 ub.add_handler(MessageHandler(func, filters), group)
             return func
-
         return decorator
 
     def set_prefix(self, user_id, prefix):
@@ -127,13 +95,13 @@ class Ubot(Client):
                     if not text.startswith(prefix):
                         continue
 
-                    without_prefix = text[len(prefix):]
+                    without_prefix = text[len(prefix) :]
 
                     for command in cmd.split("|"):
                         if not re.match(
-                                rf"^(?:{command}(?:@?{username})?)(?:\s|$)",
-                                without_prefix,
-                                flags=re.IGNORECASE | re.UNICODE,
+                            rf"^(?:{command}(?:@?{username})?)(?:\s|$)",
+                            without_prefix,
+                            flags=re.IGNORECASE | re.UNICODE,
                         ):
                             continue
 
@@ -156,25 +124,20 @@ class Ubot(Client):
         return filters.create(func)
 
     async def start(self):
-        try:
-            await super().start()
-            handler = await get_pref(self.me.id)
-            if handler:
-                self._prefix[self.me.id] = handler
-            else:
-                self._prefix[self.me.id] = [".", ",", ":", ";", "!"]
-            self._ubot.append(self)
-            self._get_my_id.append(self.me.id)
-            self._translate[self.me.id] = "id"
+        await super().start()
+        handler = await get_pref(self.me.id)
+        if handler:
+            self._prefix[self.me.id] = handler
+        else:
+            self._prefix[self.me.id] = [".", ",", ":", ";", "!"]
+        self._ubot.append(self)
+        self._get_my_id.append(self.me.id)
+        self._translate[self.me.id] = "id"
+        print(f"[𝐈𝐍𝐅𝐎] - ({self.me.id}) - 𝐒𝐓𝐀𝐑𝐓𝐄𝐃\n"
+              f"Bot name: {self.me.first_name}\n"
+              f"Bot username: {self.me.username}\n"
+              f"prefix: {', '.join(self._prefix[self.me.id])}\n")
 
-            output = f"[𝐈𝐍𝐅𝐎] - ({self.me.id}) - 𝐒𝐓𝐀𝐑𝐓𝐄𝐃\n"
-            output += f"Bot name: {self.me.first_name}\n"
-            output += f"Bot username: {self.me.username}\n"
-            output += f"prefix: {', '.join(self._prefix[self.me.id])}\n"
-
-            print(output)
-        except Exception as e:
-            logger.error(f"Error starting bot: {e}")
 
 bot = Bot(
     name="bot",
@@ -183,6 +146,7 @@ bot = Bot(
     bot_token=BOT_TOKEN,
 )
 ubot = Ubot(name="ubot")
+
 
 from PyroUbot.core.database import *
 from PyroUbot.core.function import *
