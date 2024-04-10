41eaa8fa4c2f48c24aa9bad236531f0604b4c2dd
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-11 06:32:15 +0700

    Update reseller.py

--- PyroUbot/core/database/reseller.py
+++ PyroUbot/core/database/reseller.py
@@ -4,25 +4,38 @@ resell = mongodb.seles
 
 
 async def get_seles():
-    seles = await resell.find_one({"seles": "seles"})
-    if not seles:
+    try:
+        seles = await resell.find_one({"seles": "seles"})
+        if not seles:
+            return []
+        return seles.get("reseller", [])
+    except Exception as e:
+        print(f"Error while getting resellers: {e}")
         return []
-    return seles["reseller"]
 
 
 async def add_seles(user_id):
-    reseller = await get_seles()
-    reseller.append(user_id)
-    await resell.update_one(
-        {"seles": "seles"}, {"$set": {"reseller": reseller}}, upsert=True
-    )
-    return True
+    try:
+        reseller = await get_seles()
+        reseller.append(user_id)
+        await resell.update_one(
+            {"seles": "seles"}, {"$set": {"reseller": reseller}}, upsert=True
+        )
+        return True
+    except Exception as e:
+        print(f"Error while adding reseller: {e}")
+        return False
 
 
 async def remove_seles(user_id):
-    reseller = await get_seles()
-    reseller.remove(user_id)
-    await resell.update_one(
-        {"seles": "seles"}, {"$set": {"reseller": reseller}}, upsert=True
-    )
-    return True
+    try:
+        reseller = await get_seles()
+        if user_id in reseller:
+            reseller.remove(user_id)
+            await resell.update_one(
+                {"seles": "seles"}, {"$set": {"reseller": reseller}}, upsert=True
+            )
+        return True
+    except Exception as e:
+        print(f"Error while removing reseller: {e}")
+        return False
