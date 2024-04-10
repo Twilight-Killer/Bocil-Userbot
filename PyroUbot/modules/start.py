10555a5f8d306fb4e79ae20bddb723293bec011e
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-11 06:22:39 +0700

    Update start.py

--- PyroUbot/modules/start.py
+++ PyroUbot/modules/start.py
@@ -9,3 +9,13 @@ async def _(client, message):
 @PY.BOT("start")
 async def _(client, message):
     await start_cmd(client, message)
+
+
+@PY.CALLBACK("stats")
+async def _(client, callback_query):
+    await callback_query_halder(client, callback_query)
+
+
+@PY.CALLBACK("refresh")
+async def _(client, callback_query):
+    await callback_query_refresh(client, callback_query)
