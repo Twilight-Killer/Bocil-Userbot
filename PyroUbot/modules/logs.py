2c3d4a223f2a753fe45f239997e891efe1e734c1
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-16 17:24:25 +0700

    Update logs.py

--- PyroUbot/modules/logs.py
+++ PyroUbot/modules/logs.py
@@ -1,19 +1,18 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram.enums import ChatType
-from pyrogram.file_id import FileId, FileUniqueId, FileType, FileUniqueType
 
-from PyroUbot import*
+from PyroUbot import *
 
 __MODULE__ = "logs"
 __HELP__ = """
-<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴋᴀɴɢ 』</b>
+<b>『 BANTUAN UNTUK KANG 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}logs</code> (on)
-  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴀᴋᴛɪғᴋᴀɴ ᴀᴛᴀᴜ ᴍᴇɴɴᴏɴᴀᴋᴛɪғᴋᴀɴ ᴄʜᴀɴɴᴇʟ ʟᴏɢs
+  <b>• PERINTAH:</b> <code>{0}logs</code> (on)
+  <b>• PENJELASAN:</b> untuk mengaktifkan atau menonaktifkan channel logs
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}del logs</code> 
-  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢʜᴀᴘᴜs ᴄʜᴀɴɴᴇʟ ʟᴏɢs
+  <b>• PERINTAH:</b> <code>{0}del logs</code> 
+  <b>• PENJELASAN:</b> untuk menghapus channel logs
 """
 
 @ubot.on_message(filters.group & filters.mentioned & filters.incoming, group=4)
@@ -27,72 +26,71 @@ __HELP__ = """
 async def send_logs(client, message):
     logs = await get_vars(client.me.id, "ID_LOGS")
     on_logs = await get_vars(client.me.id, "ON_LOGS")
-    if logs and on_logs:
-        psn = f"{message.text}" 
-        if message.chat.type == ChatType.PRIVATE:
-            type = "ᴘʀɪᴠᴀᴛᴇ"
-            from_user = message.chat
-            id_link = f"tg://openmessage?user_id={from_user.id}&message_id={message.id}"
-        elif message.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
-            type = "ɢʀᴏᴜᴘ"
-            from_user = message.from_user
-            id_link = message.link
-            psn = f"{message.text}"
-        rpk = f"[{from_user.first_name} {from_user.last_name or ''}](tg://user?id={from_user.id})"
-        link = f"[ᴋʟɪɴᴋ ᴅɪsɪɴɪ]({id_link})"
-
-        if message.photo:
-            try:
-                await client.send_photo(int(logs), photo= auto(), caption=f"ℹ️ ʟɪɴᴋ ᴘᴇsᴀɴ: {link}\n\n📌 ᴅɪʙᴀᴡᴀʜ ɪɴɪ ᴀᴅᴀʟᴀʜ ᴘᴇsᴀɴ ᴛᴇʀᴜsᴀɴ ᴅᴀʀɪ: {rpk}")
-            except pyrogram.errors.exceptions.bad_request_400.ChatForwardsRestricted:
-                await client.send_message(message.chat.id, "❌sᴏʀʀʏ ʙʀᴏ ʟᴏɢs ɢᴜᴀ ɢᴀᴋ ʙɪsᴀ ɴᴇʀɪᴍᴀ ᴍᴇᴅɪᴀ ᴅᴀʀɪ ʟᴜ ɢᴄ ɴʏᴀ ᴅɪ ʙᴀᴛᴀsɪ❌")
-        elif message.video:
-            try:
-                await client.send_video(int(logs), video=message.video.file_id, caption=f"ℹ️ ʟɪɴᴋ ᴘᴇsᴀɴ: {link}\n\n📌 ᴅɪʙᴀᴡᴀʜ ɪɴɪ ᴀᴅᴀʟᴀʜ ᴘᴇsᴀɴ ᴛᴇʀᴜsᴀɴ ᴅᴀʀɪ: {rpk}")
-            except pyrogram.errors.exceptions.bad_request_400.ChatForwardsRestricted:
-                await client.send_message(message.chat.id, "❌sᴏʀʀʏ ʙʀᴏ ʟᴏɢs ɢᴜᴀ ɢᴀᴋ ʙɪsᴀ ɴᴇʀɪᴍᴀ ᴍᴇᴅɪᴀ ᴅᴀʀɪ ʟᴜ ɢᴄ ɴʏᴀ ᴅɪ ʙᴀᴛᴀsɪ❌")
-        else:
-            await client.send_message(
-                int(logs),
-                f"""
-<b>📩 ᴀᴅᴀ ᴘᴇsᴀɴ ᴍᴀsᴜᴋ</b>
-    <b>•> ᴛɪᴘᴇ ᴘᴇsᴀɴ:</b> <code>{type}</code>
-    <b>•> ʟɪɴᴋ ᴘᴇsᴀɴ:</b> {link}
-    <b>•> ᴘᴇsᴀɴ:</b> {psn}
     
-<b>⤵️ ᴅɪʙᴀᴡᴀʜ ɪɴɪ ᴀᴅᴀʟᴀʜ ᴘᴇsᴀɴ ᴛᴇʀᴜsᴀɴ ᴅᴀʀɪ: {rpk}</b>
+    if not logs or not on_logs:
+        return
+    
+    if message.chat.type == ChatType.PRIVATE:
+        type = "PRIVAT"
+        from_user = message.chat
+        id_link = f"tg://openmessage?user_id={from_user.id}&message_id={message.id}"
+    elif message.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
+        type = "GRUP"
+        from_user = message.from_user
+        id_link = message.link
+        
+    rpk = f"[{from_user.first_name} {from_user.last_name or ''}](tg://user?id={from_user.id})"
+    link = f"[KLIK DISINI]({id_link})"
+
+    if message.photo:
+        photo_path = await client.download_media(message.photo.file_id)
+        try:
+            await client.send_photo(int(logs), photo=photo_path, caption=f"ℹ️ ʟɪɴᴋ ᴘᴇsᴀɴ: {link}\n\n📌 ᴅɪʙᴀᴡᴀʜ ɪɴɪ ᴀᴅᴀʟᴀʜ ᴘᴇsᴀɴ ᴛᴇʀᴜsᴀɴ ᴅᴀʀɪ: {rpk}")
+        except pyrogram.errors.exceptions.bad_request_400.ChatForwardsRestricted:
+            await client.send_message(message.chat.id, "❌Maaf, bot tidak bisa menerima media dari grup Anda karena dibatasi❌")
+    elif message.video:
+        file_id = message.video.file_id
+        try:
+            await client.send_video(int(logs), video=file_id, caption=f"ℹ️ ʟɪɴᴋ ᴘᴇsᴀɴ: {link}\n\n📌 ᴅɪʙᴀᴡᴀʜ ɪɴɪ ᴀᴅᴀʟᴀʜ ᴘᴇsᴀɴ ᴛᴇʀᴜsᴀɴ ᴅᴀʀɪ: {rpk}")
+        except pyrogram.errors.exceptions.bad_request_400.ChatForwardsRestricted:
+            await client.send_message(message.chat.id, "❌Maaf, bot tidak bisa menerima media dari grup Anda karena dibatasi❌")
+    else:
+        await client.send_message(
+            int(logs),
+            f"""
+<b>📩 Ada pesan masuk</b>
+    <b>•> Tipe pesan:</b> <code>{type}</code>
+    <b>•> Link pesan:</b> {link}
+    <b>•> Pesan:</b> {message.text}
+    
+<b>⤵️ Dibawah ini adalah pesan terusan dari: {rpk}</b>
 """,
-            )
-          
+        )
+      
 @PY.UBOT("logs")
 async def set_logs(client, message: Message):
     if len(message.command) < 2:
         return await message.reply(
-            "ʜᴀʀᴀᴘ ʙᴀᴄᴀ ᴍᴇɴᴜ ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴍᴇɴɢᴇᴛᴀʜᴜɪ ᴄᴀʀᴀ ᴘᴇɴɢɢụ"
+            "Harap baca menu bantuan untuk mengetahui cara penggunaan"
         )
 
-    query = {"on": True}
     command = message.command[1].lower()
 
-    if command not in query:
-        return await message.reply("ᴏᴘsɪ ᴛɪᴅᴀᴋ ᴠᴀʟɪᴅ. ʜᴀʀᴀᴘ ɢᴜɴᴀᴋᴀɴ 'on'.")
+    if command != "on":
+        return await message.reply("Opsi tidak valid. Harap gunakan 'on'.")
 
-    value = query[command]
-
-    vars = await get_vars(client.me.id, "ID_LOGS")
-
-    if not vars:
-        logs = await create_logs(client)
-        await set_vars(client.me.id, "ID_LOGS", logs)
-
-    await set_vars(client.me.id, "ON_LOGS", value)
+    logs = await create_logs(client)
+    await set_vars(client.me.id, "ID_LOGS", logs.id)
+    await set_vars(client.me.id, "ON_LOGS", True)
+    
     return await message.reply(
-        f"<b>✅ <code>LOGS</code> ʙᴇʀʜᴀsɪʟ ᴅɪsᴇᴛᴛɪɴɢ ᴋᴇ:</b> <code>{value}</code>"
+        f"<b>✅ <code>LOGS</code> berhasil diaktifkan</b>"
     )
 
 @PY.UBOT("del logs")
 async def del_logs(client, message: Message):
     logs = await get_vars(client.me.id, "ID_LOGS")
+    
     if not logs:
         return await message.reply("Logs belum diatur.")
 
@@ -100,15 +98,18 @@ async def del_logs(client, message: Message):
         await client.leave_chat(int(logs))
         await set_vars(client.me.id, "ID_LOGS", None)
         await set_vars(client.me.id, "ON_LOGS", False)
-        return await message.reply("ʟᴏɢs ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs❌")
+        return await message.reply("Logs berhasil dihapus❌")
     except Exception as e:
         return await message.reply(f"Gagal menghapus logs: {e}")
 
 async def create_logs(client):
-    url = wget.download("https://graph.org/file/5b61f029143eadf42020e.jpg")
+    photo_url = "https://graph.org/file/5b61f029143eadf42020e.jpg"  # URL foto profil saluran log
+    photo_path = await client.download_media(photo_url)
+    
     logs = await client.create_channel(f"logs: {client.me.username}")
     await client.set_chat_photo(
         logs.id,
-        photo=url,
+        photo=photo_path,
     )
-    return logs.id
+    
+    return logs
