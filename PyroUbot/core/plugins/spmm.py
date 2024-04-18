19ed48caa8a1997818977cd8a8bfd12d2f5f10bb
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-18 23:41:20 +0700

    Update spmm.py

--- PyroUbot/core/plugins/spmm.py
+++ PyroUbot/core/plugins/spmm.py
@@ -1,11 +1,10 @@
 import asyncio
 
-spam_id = []
-dspam_id = []
+dspam_processes = {}  # Dictionary to store information about dspam processes
 
 async def spam_cmd(client, message):
     reply = message.reply_to_message
-    msg = await message.reply("sᴇᴅᴀɴɢ ᴅɪᴘʀᴏsᴇs", quote=False)
+    msg = await message.reply("Sedang diproses", quote=False)
     if reply:
         try:
             count_message = int(message.command[1])
@@ -16,9 +15,7 @@ async def spam_cmd(client, message):
             return await msg.edit(str(error))
     else:
         if len(message.command) < 2:
-            return await msg.edit(
-                "sɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ <code>.help spam</code> ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ᴄᴀʀᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ"
-            )
+            return await msg.edit("Silakan ketik <code>.help spam</code> untuk melihat cara menggunakan perintah ini")
         else:
             try:
                 count_message = int(message.command[1])
@@ -30,77 +27,64 @@ async def spam_cmd(client, message):
     await msg.delete()
     await message.delete()
 
-
 async def dspam_cmd(client, message):
     reply = message.reply_to_message
-    msg = await message.reply("sᴇᴅᴀɴɢ ᴅɪᴘʀᴏsᴇs", quote=False)
+    msg = await message.reply("Sedang diproses", quote=False)
     if reply:
         try:
             count_message = int(message.command[1])
             count_delay = int(message.command[2])
         except Exception as error:
             return await msg.edit(str(error))
-        dspam_id.append(message.chat.id)
+        dspam_processes[message.chat.id] = {"count_message": count_message, "count_delay": count_delay, "text_spam": reply.text}
         for i in range(count_message):
             try:
                 await reply.copy(message.chat.id)
                 await asyncio.sleep(count_delay)
             except:
                 pass
-        dspam_id.remove(message.chat.id)
+        dspam_processes.pop(message.chat.id, None)
     else:
         if len(message.command) < 4:
-            return await msg.edit(
-                "sɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ <code>.help spam</code> ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ᴄᴀʀᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ"
-            )
+            return await msg.edit("Silakan ketik <code>.help spam</code> untuk melihat cara menggunakan perintah ini")
         else:
             try:
                 count_message = int(message.command[1])
                 count_delay = int(message.command[2])
             except Exception as error:
                 return await msg.edit(str(error))
-            dspam_id.append(message.chat.id)
+            dspam_processes[message.chat.id] = {"count_message": count_message, "count_delay": count_delay, "text_spam": message.text.split(None, 3)[3]}
             for i in range(count_message):
                 try:
                     await message.reply(message.text.split(None, 3)[3], quote=False)
                     await asyncio.sleep(count_delay)
                 except:
                     pass
-            dspam_id.remove(message.chat.id)
+            dspam_processes.pop(message.chat.id, None)
     await msg.delete()
     await message.delete()
 
-
 async def list_dspam(client, message):
-    if not dspam_id:
+    if not dspam_processes:
         return await message.reply("Tidak ada proses dspam yang sedang berjalan.")
     text = "Proses dspam sedang berjalan di grup ini:\n"
-    for chat_id, _, count_message, count_delay, text_spam in dspam_id:
-        try:
-            chat = await client.get_chat(chat_id)
-            text += f"- {chat.title} (Penundaan: {count_delay} detik, Kata-kata: {text_spam})\n"
-        except:
-            pass
+    for chat_id, process_info in dspam_processes.items():
+        text += f"- {chat_id} (Penundaan: {process_info['count_delay']} detik, Kata-kata: {process_info['text_spam']})\n"
     await message.reply(text)
 
-
 async def cancel_dspam(client, message):
-    if message.chat.id not in dspam_id:
+    if message.chat.id not in dspam_processes:
         return await message.reply("Tidak ada proses dspam yang sedang berjalan di grup ini.")
-    dspam_id.remove(message.chat.id)
+    dspam_processes.pop(message.chat.id, None)
     await message.reply("Proses dspam berhasil dibatalkan.")
-
 
 async def addtext_cmd(client, message):
     if len(message.command) < 2:
         return await message.reply("Silakan berikan kata-kata yang ingin ditambahkan.")
     
     new_text = ' '.join(message.command[1:])
-    for i, (chat_id, _, _, _, old_text) in enumerate(dspam_id):
-        if chat_id == message.chat.id:
-            dspam_id[i] = (chat_id, _, _, _, old_text + f" {new_text}")
-            break
+    if message.chat.id in dspam_processes:
+        dspam_processes[message.chat.id]["text_spam"] += f" {new_text}"
+        return await message.reply(f"Kata-kata '{new_text}' berhasil ditambahkan ke dalam proses dspam.")
     else:
         return await message.reply("Tidak ada proses dspam yang sedang berjalan di grup ini.")
-    
-    return await message.reply(f"Kata-kata '{new_text}' berhasil ditambahkan ke dalam proses dspam.")
