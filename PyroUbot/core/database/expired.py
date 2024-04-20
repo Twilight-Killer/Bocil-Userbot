5af21f8918a73706fafac96d0b0b1e4b8ec77a57
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-20 11:08:33 +0700

    Update expired.py

--- PyroUbot/core/database/expired.py
+++ PyroUbot/core/database/expired.py
@@ -4,7 +4,7 @@ userEXP = mongo_client["PyroUbot"]["users"]
 
 
 async def get_expired_date(user_id):
-    user = await userEXP.users.find_one({"_id": user_id})
+    user = await userEXP.find_one({"_id": user_id})
     if user:
         return user.get("expire_date")
     else:
@@ -12,12 +12,12 @@ async def get_expired_date(user_id):
 
 
 async def set_expired_date(user_id, expire_date):
-    await userEXP.users.update_one(
+    await userEXP.update_one(
         {"_id": user_id}, {"$set": {"expire_date": expire_date}}, upsert=True
     )
 
 
 async def rem_expired_date(user_id):
-    await userEXP.users.update_one(
+    await userEXP.update_one(
         {"_id": user_id}, {"$unset": {"expire_date": ""}}, upsert=True
     )
