6206623eb494d332291a1182a3b09acbf5697431
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-20 19:46:48 +0700

    Update qtl.py

--- PyroUbot/core/plugins/qtl.py
+++ PyroUbot/core/plugins/qtl.py
@@ -28,19 +28,20 @@ async def quotly_cmd(client, message):
         try:
             for x in msg:
                 await x.forward("@QuotLyBot")
-        except Exception:
-            pass
+        except Exception as error:
+            return await info.edit(str(error))
+
         await asyncio.sleep(9)
         await info.delete()
-        async for quotly in client.get_chat_history("@QuotLyBot", limit=1):
+        async for quotly in client.iter_history("@QuotLyBot", limit=1):
             if not quotly.sticker:
-                await message.reply(
-                    f"❌ @QuotLyBot ᴛɪᴅᴀᴋ ᴅᴀᴘᴀᴛ ᴍᴇʀᴇsᴘᴏɴ ᴘᴇʀᴍɪɴᴛᴀᴀɴ", quote=True
+                return await message.reply(
+                    "❌ @QuotLyBot ᴛɪᴅᴀᴋ ᴅᴀᴘᴀᴛ ᴍᴇʀᴇsᴘᴏɴ ᴘᴇʀᴍɪɴᴛᴀᴀɴ", quote=True
                 )
-            else:
-                sticker = await client.download_media(quotly)
-                await message.reply_sticker(sticker, quote=True)
-                os.remove(sticker)
+
+            sticker = await quotly.download()
+            await message.reply_sticker(sticker, quote=True)
+            os.remove(sticker)
     else:
         if len(message.command) < 2:
             return await info.edit("<b>ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ/ᴍᴇᴅɪᴀ</b>")
@@ -49,9 +50,10 @@ async def quotly_cmd(client, message):
                 "@QuotLyBot", f"/qcolor {message.command[1]}"
             )
             await asyncio.sleep(1)
-            get = await client.get_messages("@QuotLyBot", msg.id + 1)
+            get = await msg.reply_to_message
             await info.edit(
                 f"<b>ᴡᴀʀɴᴀ ʟᴀᴛᴀʀ ʙᴇʟᴀᴋᴀɴɢ ᴋᴜᴛɪᴘᴀɴ ᴅɪsᴇᴛᴇʟ ᴋᴇ:</b> <code>{get.text.split(':')[1]}</code>"
             )
+
     user_info = await client.resolve_peer("@QuotLyBot")
-    return await client.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
+    return await client.send(DeleteHistory(peer=user_info, max_id=0, revoke=True))
