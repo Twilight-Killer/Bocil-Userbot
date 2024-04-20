f8e78457c5a6d8de9d210b6e1ba7d9b3757217a3
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-20 17:33:48 +0700

    Update pgr.py

--- PyroUbot/core/plugins/pgr.py
+++ PyroUbot/core/plugins/pgr.py
@@ -1,28 +1,27 @@
 import asyncio
 
-
 async def del_cmd(client, message):
     rep = message.reply_to_message
     await message.delete()
     await rep.delete()
 
-
 async def purgeme_cmd(client, message):
+    await message.delete()
     if len(message.command) != 2:
-        return await message.delete()
+        return
     n = (
         message.reply_to_message
         if message.reply_to_message
         else message.text.split(None, 1)[1].strip()
     )
     if not n.isnumeric():
-        return await message.reply("ᴀʀɢᴜᴍᴇɴ ᴛɪᴅᴀᴋ ᴠᴀʟɪᴅ")
+        return await message.reply("Argument tidak valid")
     n = int(n)
     if n < 1:
-        return await message.reply("ʙᴜᴛᴜʜ ɴᴏᴍᴇʀ >=1-999")
+        return await message.reply("Nomor harus >=1-999")
     chat_id = message.chat.id
     message_ids = [
-        m.id
+        m.message_id
         async for m in client.search_messages(
             chat_id,
             from_user=int(message.from_user.id),
@@ -30,28 +29,27 @@ async def purgeme_cmd(client, message):
         )
     ]
     if not message_ids:
-        return await message.reply_text("ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴘᴇsᴀɴ ʏᴀɴɢ ᴅɪᴛᴇᴍᴜᴋᴀɴ")
-    to_delete = [message_ids[i : i + 999] for i in range(0, len(message_ids), 999)]
+        return await message.reply("Tidak ada pesan yang ditemukan")
+    to_delete = [message_ids[i : i + 100] for i in range(0, len(message_ids), 100)]
     for hundred_messages_or_less in to_delete:
         await client.delete_messages(
             chat_id=chat_id,
             message_ids=hundred_messages_or_less,
             revoke=True,
         )
-        mmk = await message.reply(f"✅ {n} ᴘᴇsᴀɴ ᴛᴇʟᴀʜ ᴅɪ ʜᴀᴘᴜs")
+        mmk = await message.reply(f"{n} pesan telah dihapus")
         await asyncio.sleep(2)
         await mmk.delete()
-
 
 async def purge_cmd(client, message):
     await message.delete()
     if not message.reply_to_message:
-        return await message.reply_text("ᴍᴇᴍʙᴀʟᴀs ᴘᴇsᴀɴ ᴜɴᴛᴜᴋ ᴅɪʙᴇʀsɪʜᴋᴀɴ.")
+        return await message.reply("Membalas pesan untuk dihapus.")
     chat_id = message.chat.id
     message_ids = []
     for message_id in range(
-        message.reply_to_message.id,
-        message.id,
+        message.reply_to_message.message_id,
+        message.message_id,
     ):
         message_ids.append(message_id)
         if len(message_ids) == 100:
