972e6a58832a7b0e46281044dfe5128c0b36b33a
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-20 11:14:40 +0700

    Update userbot.py

--- PyroUbot/core/database/userbot.py
+++ PyroUbot/core/database/userbot.py
@@ -1,12 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from PyroUbot.config import MONGO_URL
 
-ubotdb = mongodb.ubot
+mongo_client = AsyncIOMotorClient(MONGO_URL)
+ubotdb = mongo_client["PyroUbot"]["ubot"]
 
 async def get_user_data():
-    """
-    Get user data from the database.
-    """
     data = []
     async for ubot in ubotdb.find({"user_id": {"$exists": 1}}):
         data.append({
@@ -18,9 +16,6 @@ async def get_user_data():
     return data
 
 async def add_ubot(user_id, api_id, api_hash, session_string):
-    """
-    Add a userbot to the database.
-    """
     await ubotdb.update_one(
         {"user_id": user_id},
         {"$set": {"api_id": api_id, "api_hash": api_hash, "session_string": session_string}},
@@ -28,15 +23,9 @@ async def add_ubot(user_id, api_id, api_hash, session_string):
     )
 
 async def remove_ubot(user_id):
-    """
-    Remove a userbot from the database.
-    """
     await ubotdb.delete_one({"user_id": user_id})
 
 async def get_userbots():
-    """
-    Get all userbots from the database.
-    """
     data = []
     async for ubot in ubotdb.find({"user_id": {"$exists": 1}}):
         data.append(
