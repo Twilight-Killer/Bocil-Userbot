5de856e610b389d11011b405fb49549cb5e102e3
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-20 11:22:11 +0700

    Update notes.py

--- PyroUbot/core/database/notes.py
+++ PyroUbot/core/database/notes.py
@@ -2,17 +2,19 @@ from PyroUbot.core.database import mongo_client
 
 collection = mongo_client["PyroUbot"]["notes"]
 
-
 async def save_note(user_id, note_name, message):
     doc = {"_id": user_id, "notes": {note_name: message}}
-    result = await collection.find_one({"_id": user_id})
-    if result:
-        await collection.update_one(
-            {"_id": user_id}, {"$set": {f"notes.{note_name}": message}}
-        )
-    else:
-        await collection.insert_one(doc)
-
+    try:
+        result = await collection.find_one({"_id": user_id})
+        if result:
+            await collection.update_one(
+                {"_id": user_id}, {"$set": {f"notes.{note_name}": message}}
+            )
+        else:
+            await collection.insert_one(doc)
+    except:
+        return False
+    return True
 
 async def get_note(user_id, note_name):
     result = await collection.find_one({"_id": user_id})
@@ -22,25 +24,29 @@ async def get_note(user_id, note_name):
             return note_id
         except KeyError:
             return None
-    else:
-        return None
-
+    return None
 
 async def rm_note(user_id, note_name):
-    await collection.update_one(
-        {"_id": user_id}, {"$unset": {f"notes.{note_name}": ""}}
-    )
-
+    try:
+        await collection.update_one(
+            {"_id": user_id}, {"$unset": {f"notes.{note_name}": ""}}
+        )
+        return True
+    except:
+        return False
 
 async def all_notes(user_id):
-    results = await collection.find_one({"_id": user_id})
     try:
-        notes_dic = results["notes"]
+        results = await collection.find_one({"_id": user_id})
+        notes_dic = results.get("notes", {})
         key_list = notes_dic.keys()
         return key_list
     except:
         return None
 
-
 async def rm_all(user_id):
-    await collection.update_one({"_id": user_id}, {"$unset": {"notes": ""}})
+    try:
+        await collection.update_one({"_id": user_id}, {"$unset": {"notes": ""}})
+        return True
+    except:
+        return False
