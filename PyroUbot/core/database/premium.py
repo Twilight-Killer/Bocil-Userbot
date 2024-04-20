130a5e8bb8e3a3c4f929e44ec917a9a11ddc416f
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-20 11:21:27 +0700

    Update premium.py

--- PyroUbot/core/database/premium.py
+++ PyroUbot/core/database/premium.py
@@ -2,23 +2,28 @@ from PyroUbot.core.database import mongodb
 
 user = mongodb.premium
 
-
 async def get_prem():
     prem = await user.find_one({"prem": "prem"})
     if not prem:
         return []
-    return prem["list"]
-
+    return prem.get("list", [])
 
 async def add_prem(user_id):
-    list = await get_prem()
-    list.append(user_id)
-    await user.update_one({"prem": "prem"}, {"$set": {"list": list}}, upsert=True)
-    return True
-
+    try:
+        list_ = await get_prem()
+        list_.append(user_id)
+        await user.update_one({"prem": "prem"}, {"$set": {"list": list_}}, upsert=True)
+        return True
+    except:
+        return False
 
 async def remove_prem(user_id):
-    list = await get_prem()
-    list.remove(user_id)
-    await user.update_one({"prem": "prem"}, {"$set": {"list": list}}, upsert=True)
-    return True
+    try:
+        list_ = await get_prem()
+        if user_id in list_:
+            list_.remove(user_id)
+            await user.update_one({"prem": "prem"}, {"$set": {"list": list_}}, upsert=True)
+            return True
+        return False
+    except:
+        return False
