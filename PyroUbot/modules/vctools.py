afa0e4551cdfd02eae4a698b10ce893aa01b17b7
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-23 01:14:06 +0700

    Update vctools.py

--- PyroUbot/modules/vctools.py
+++ PyroUbot/modules/vctools.py
@@ -1,8 +1,3 @@
-"""
-yang hapus credits pantatnya bisulan
-create by: https://t.me/NorSodikin 
-"""
-
 import asyncio
 from random import randint
 
@@ -38,19 +33,21 @@ def remove_list(user_id):
 
 
 def add_list(client, chat_id):
-    data = {
-        "id": client.me.id,
-        "nama": f"• <b>[{client.me.first_name} {client.me.last_name or ''}](tg://user?id={client.me.id})</b> | <code>{chat_id}</code>",
-    }
-    list_data.append(data)
+    async def add_list_async(client, chat_id):
+        chat = await client.get_chat(chat_id)
+        data = {
+            "id": client.me.id,
+            "nama": f"• <b>[{client.me.first_name} {client.me.last_name or ''}](tg://user?id={client.me.id})</b> di <code>{chat.title}</code>",
+        }
+        list_data.append(data)
 
+    asyncio.create_task(add_list_async(client, chat_id))
 
 def get_list():
     if not list_data:
-        return "<b>ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴜsᴇʀ ᴅɪ ᴅᴀʟᴀᴍ ᴏʙʀᴏʟᴀɴ sᴜᴀʀᴀ ᴍᴀɴᴀᴘᴜɴ</b>"
+        return "Tidak ada pengguna dalam obrolan suara saat ini."
 
-    msg = "\n".join(item["nama"] for item in list_data)
-    return msg
+    return "\n".join(item["nama"] for item in list_data)
 
 
 async def get_group_call(client, message):
@@ -116,11 +113,12 @@ async def _(client, message):
 async def _(client, message):
     msg = await message.reply("<b>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ....</b>")
     chat_id = message.command[1] if len(message.command) > 1 else message.chat.id
+    chat_title = message.chat.title
     try:
         await client.group_call.start(chat_id, join_as=client.me.id)
     except Exception as e:
         return await msg.edit(f"ERROR: {e}")
-    await msg.edit("<b>ʙᴇʀʜᴀsɪʟ ɴᴀɪᴋ ᴋᴇ ᴏʙʀᴏʟᴀɴ sᴜᴀʀᴀ</b>")
+    await msg.edit(f"<b>ʙᴇʀʜᴀsɪʟ ɴᴀɪᴋ ᴋᴇ ᴏʙʀᴏʟᴀɴ sᴜᴀʀᴀ</b>\n<b>ɢʀᴏᴜᴘ : </b><code>{chat_title}</code>")
     await asyncio.sleep(5)
     await client.group_call.set_is_mute(True)
     add_list(client, chat_id)
@@ -129,14 +127,16 @@ async def _(client, message):
 @PY.UBOT("leavevc")
 async def _(client, message):
     msg = await message.reply("<b>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ....</b>")
+    chat_title = message.chat.title
     try:
         await client.group_call.stop()
     except Exception as e:
         return await msg.edit(f"ERROR: {e}")
     remove_list(client.me.id)
-    return await msg.edit("<b>ʙᴇʀʜᴀsɪʟ ᴛᴜʀᴜɴ ᴅᴀʀɪ ᴏʙʀᴏʟᴀɴ sᴜᴀʀᴀ</b>")
+    return await msg.edit(f"<b>ʙᴇʀʜᴀsɪʟ ᴛᴜʀᴜɴ ᴅᴀʀɪ ᴏʙʀᴏʟᴀɴ sᴜᴀʀᴀ</b>\n<b>ɢʀᴏᴜᴘ : </b><code>{chat_title}</code>")
 
-
-@PY.UBOT("listvc", FILTERS.ME_OWNER)
+@PY.UBOT("listvc")
 async def _(client, message):
-    await message.reply(get_list())
+    chat_title = message.chat.title
+    voice_chat_list = get_list()
+    await message.reply(f"<b>Daftar Pengguna dalam Obrolan Suara {chat_title}:</b>\n\n{voice_chat_list}")
