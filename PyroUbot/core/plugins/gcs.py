8c8a3e9d4432897aada4c6cfc51d1f4c0dd85cd0
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-23 10:12:31 +0700

    Update gcs.py

--- PyroUbot/core/plugins/gcs.py
+++ PyroUbot/core/plugins/gcs.py
@@ -20,7 +20,7 @@ async def broadcast_group_cmd(client, message):
     send = get_message(message)
     if not send:
         return await msg.edit(
-            f"<b>{reply_emoji}Mohon balas sesuatu atau ketik sesuatu" if client.me.is_premium else "Mohon balas sesuatu atau ketik sesuatu<b>")
+            f"<b>{reply_emoji}Mohon balas sesuatu atau ketik sesuatu" if client.me.is_premium else "🔁Mohon balas sesuatu atau ketik sesuatu<b>")
 
     chats = await get_global_id(client, "group")
     blacklist = await get_chat(client.me.id)
