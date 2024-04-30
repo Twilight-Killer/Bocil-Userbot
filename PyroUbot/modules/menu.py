82ca08169507aa94920bd90c4bd0140bde5ba706
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-04-30 23:01:39 +0700

    Update menu.py

--- PyroUbot/modules/menu.py
+++ PyroUbot/modules/menu.py
@@ -2,6 +2,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from PyroUbot import *
 
+
 @PY.BOT("control")
 async def start_command(client, message):
     start_message = "𝐬𝐢𝐥𝐚𝐤𝐚𝐧 𝐩𝐢𝐥𝐢𝐡 𝐩𝐞𝐫𝐢𝐧𝐭𝐚𝐡 𝐲𝐚𝐧𝐠 𝐢𝐧𝐠𝐢𝐧 𝐚𝐧𝐝𝐚 𝐠𝐮𝐧𝐚𝐤𝐚𝐧 ⏬"
@@ -10,38 +11,48 @@ async def start_command(client, message):
     keyboard = InlineKeyboardMarkup([[menu_button, url_button]])
     await message.reply_text(start_message, reply_markup=keyboard)
 
-@PY.CALLBACK("command1")
-async def command1_callback(client, callback_query):
-    back_button = InlineKeyboardButton("Back", callback_data="menu")
-    keyboard = InlineKeyboardMarkup([[back_button]])
-    await callback_query.edit_message_text("HA•• MUH••• RI•••:085748237211", reply_markup=keyboard)
 
-@PY.CALLBACK("command2")
-async def command2_callback(client, callback_query):
-    back_button = InlineKeyboardButton("Back", callback_data="menu")
-    keyboard = InlineKeyboardMarkup([[back_button]])
-    await callback_query.edit_message_text("KONTOL BELUM ADA ", reply_markup=keyboard)
+@PY.CALLBACK("dana")
+async def dana_callback(client, callback_query):
+    dana_text = """💰dana
+ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ᴍᴇʟᴀʟᴜɪ ᴅᴀɴᴀ ɢᴜɴᴀᴋᴀɴ ɴᴏᴍᴏʀ ᴛᴜᴊᴜᴀɴ ʙᴇʀɪᴋᴜᴛ:
+085748237211 ᴀ/ɴ ʜᴀᴍxxxx Mᴜʜᴀᴍxx ʀɪᴅxx
+sᴇᴛᴇʟᴀʜ ᴀɴᴅᴀ ᴍᴇʟᴀᴋᴜᴋᴀɴ ᴘᴇᴍʙᴀʏᴀʀᴀɴ,
+ᴛᴇᴋᴀɴ ᴛᴏᴍʙᴏʟ ʙᴀᴄᴋ ᴅɪʙᴀᴡᴀʜ ɪɴɪ ᴜɴᴛᴜᴋ ᴋᴇᴍʙᴀʟɪ"""
+    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Back", callback_data="menu")]])
+    await callback_query.edit_message_text(dana_text, reply_markup=keyboard)
+    
 
-@PY.CALLBACK("command3")
-async def command3_callback(client, callback_query):
-    back_button = InlineKeyboardButton("Back", callback_data="menu")
-    keyboard = InlineKeyboardMarkup([[back_button]])
-    await callback_query.edit_message_text("MEMEK BELUM ADA JUGA ", reply_markup=keyboard)
+@PY.CALLBACK("ovo")
+async def dana_callback(client, callback_query):
+    ovo_text = """💰ᴏᴠᴏ
+ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ᴍᴇʟᴀʟᴜɪ ᴏᴠᴏ ɢᴜɴᴀᴋᴀɴ ɴᴏᴍᴏʀ ᴛᴜᴊᴜᴀɴ ʙᴇʀɪᴋᴜᴛ:
+085748237211 ᴀ/ɴ ʜᴀᴍxxxx Mᴜʜᴀᴍxx ʀɪᴅxx
+sᴇᴛᴇʟᴀʜ ᴀɴᴅᴀ ᴍᴇʟᴀᴋᴜᴋᴀɴ ᴘᴇᴍʙᴀʏᴀʀᴀɴ,
+ᴛᴇᴋᴀɴ ᴛᴏᴍʙᴏʟ ʙᴀᴄᴋ ᴅɪʙᴀᴡᴀʜ ɪɴɪ ᴜɴᴛᴜᴋ ᴋᴇᴍʙᴀʟɪ"""
+    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Back", callback_data="menu")]])
+    await callback_query.edit_message_text(ovo_text, reply_markup=keyboard)
 
-@PY.CALLBACK("command4")
-async def command4_callback(client, callback_query):
-    back_button = InlineKeyboardButton("Back", callback_data="menu")
-    keyboard = InlineKeyboardMarkup([[back_button]])
-    await callback_query.edit_message_text("YAH KEPO YA", reply_markup=keyboard)
+
+@PY.CALLBACK("gopay")
+async def dana_callback(client, callback_query):
+    gopay_text = """💰gopay
+ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ᴍᴇʟᴀʟᴜɪ ɢᴏᴘᴀʏ ɢᴜɴᴀᴋᴀɴ ɴᴏᴍᴏʀ ᴛᴜᴊᴜᴀɴ ʙᴇʀɪᴋᴜᴛ:
+085748237211 ᴀ/ɴ ʜᴀᴍxxxx Mᴜʜᴀᴍxx ʀɪᴅxx
+sᴇᴛᴇʟᴀʜ ᴀɴᴅᴀ ᴍᴇʟᴀᴋᴜᴋᴀɴ ᴘᴇᴍʙᴀʏᴀʀᴀɴ,
+ᴛᴇᴋᴀɴ ᴛᴏᴍʙᴏʟ ʙᴀᴄᴋ ᴅɪʙᴀᴡᴀʜ ɪɴɪ ᴜɴᴛᴜᴋ ᴋᴇᴍʙᴀʟɪ"""
+    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Back", callback_data="menu")]])
+    await callback_query.edit_message_text(gopay_text, reply_markup=keyboard)
+
 
 @PY.CALLBACK("menu")
 async def menu_callback(client, callback_query):
     keyboard = InlineKeyboardMarkup([
         [InlineKeyboardButton("sᴛᴀᴛs 🇲🇨", callback_data="stats")],
-        [InlineKeyboardButton("ᴅᴀɴᴀ 💳", callback_data="command1"),
+        [InlineKeyboardButton("ᴅᴀɴᴀ 💳", callback_data="dana"),
          InlineKeyboardButton("ʀᴇsᴛᴀʀᴛ ✅", callback_data="control")],
-        [InlineKeyboardButton("ʙᴇʟɪ ʙᴏᴛ 📩", callback_data="command3"),
-         InlineKeyboardButton("ᴍᴇᴍᴇᴋ", callback_data="command4")],
+        [InlineKeyboardButton("ᴏᴠᴏ 📩", callback_data="ovo"),
+         InlineKeyboardButton("ɢᴏᴘᴀʏ", callback_data="gopay")],
         [InlineKeyboardButton("🔁", callback_data="back")]
     ])
     await callback_query.edit_message_text("HALO MEK PENCET MENU DI BAWAH YA ADA PILAHAN YANG LU MAU :", reply_markup=keyboard)
