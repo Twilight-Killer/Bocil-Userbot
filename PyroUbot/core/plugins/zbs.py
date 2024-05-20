2baa4e9e8a850a56840ba7ff891d046464d84921
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-05-20 17:33:51 +0700

    Update zbs.py

--- PyroUbot/core/plugins/zbs.py
+++ PyroUbot/core/plugins/zbs.py
@@ -2,7 +2,7 @@ async def zombies_cmd(client, message):
     chat_id = message.chat.id
     deleted_users = []
     banned_users = 0
-    Tm = await message.reply("<code>sᴇᴅᴀɴɢ ᴍᴇᴍᴇʀɪᴋsᴀ</code>")
+    Tm = await message.reply("<code>memeriksa....</code>")
     async for i in client.get_chat_members(chat_id):
         if i.user.is_deleted:
             deleted_users.append(i.user.id)
@@ -13,6 +13,6 @@ async def zombies_cmd(client, message):
                 await message.chat.ban_member(deleted_user)
             except Exception:
                 pass
-        await Tm.edit(f"<b>ʙᴇʀʜᴀsɪʟ ᴍᴇɴɢᴇʟᴜᴀʀᴋᴀɴ {banned_users} ᴀᴋᴜɴ ᴛᴇʀʜᴀᴘᴜs</b>")
+        await Tm.edit(f"<b>berhasil mengeluarkan {banned_users} akun terhapus</b>")
     else:
-        await Tm.edit("<b>ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴀᴋᴜɴ ᴛᴇʀʜᴀᴘᴜs ᴅɪ ɢʀᴏᴜᴘ ɪɴɪ</b>")
+        await Tm.edit("<b>tidak ada akun terhapus di group ini</b>")
