32a3e91177e42975e1f91094a2eb9b7c31cc9f87
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-05-20 16:56:25 +0700

    Update smd.py

--- PyroUbot/core/plugins/smd.py
+++ PyroUbot/core/plugins/smd.py
@@ -6,13 +6,13 @@ from pyrogram.raw.functions.messages import DeleteHistory
 async def sosmed_cmd(client, message):
     if len(message.command) < 2:
         return await message.reply(
-            f"<code>{message.text}</code> ʟɪɴᴋ ʏᴛ/ɪɢ/ꜰʙ/ᴛᴡ/ᴛɪᴋᴛᴏᴋ"
+            f"<code>{message.text}</code> link yt/ig/fb/tw/tiktok"
         )
     else:
         bot = "thisvidbot"
         link = message.text.split()[1]
         await client.unblock_user(bot)
-        Tm = await message.reply("<code>ᴘʀᴏᴄᴇssɪɴɢ . . .</code>")
+        Tm = await message.reply("<code>prosesss...</code>")
         xnxx = await client.send_message(bot, link)
         await asyncio.sleep(10)
         try:
@@ -21,7 +21,7 @@ async def sosmed_cmd(client, message):
             await Tm.delete()
         except Exception:
             await Tm.edit(
-                "<b>ᴠɪᴅᴇᴏ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ sɪʟᴀʜᴋᴀɴ ᴜʟᴀɴɢɪ ʙᴇʙᴇʀᴀᴘᴀ sᴀᴀᴛ ʟᴀɢɪ</b>"
+                "<b>video tidak ditemukan silahksn coba lagi beberapa saat nantik kepeseng</b>"
             )
         user_info = await client.resolve_peer(bot)
         return await client.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
