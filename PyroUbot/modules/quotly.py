537ced2135e982508de62eb189d537051c8dd157
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-05-20 12:31:26 +0700

    Update quotly.py

--- PyroUbot/modules/quotly.py
+++ PyroUbot/modules/quotly.py
@@ -2,13 +2,21 @@ from PyroUbot import *
 
 __MODULE__ = "quotly"
 __HELP__ = """
-<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ǫᴜᴏᴛʟʏ 』</b>
+<b>『 bantuan quotly 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}q</code> [ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ]
-  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> rubah text jadi stiker
+  <b>• perintah:</b> <code>{0}q</code> [reply to text]
+  <b>• penjelasan:</b> rubah text jadi stiker
+
+  <b>• perintah:</b> <code>{0}kang</code> [reply to img/stiker]
+  <b>• penjelasan:</b> untuk custom stiker/text
 """
 
 
 @PY.UBOT("q")
 async def _(client, message):
     await quotly_cmd(client, message)
+
+
+@PY.UBOT("kang")
+async def _(client, message):
+    await kang_cmd(client, message)
