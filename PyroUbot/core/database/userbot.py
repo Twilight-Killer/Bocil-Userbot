847745ba2db17d980d1539d21c1194f5a557840a
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-05-23 02:00:46 +0700

    Update userbot.py

--- PyroUbot/core/database/userbot.py
+++ PyroUbot/core/database/userbot.py
@@ -4,16 +4,6 @@ from PyroUbot.config import MONGO_URL
 mongo_client = AsyncIOMotorClient(MONGO_URL)
 ubotdb = mongo_client["PyroUbot"]["ubot"]
 
-async def get_user_data():
-    data = []
-    async for ubot in ubotdb.find({"user_id": {"$exists": 1}}):
-        data.append({
-            "name": str(ubot["user_id"]),
-            "api_id": ubot["api_id"],
-            "api_hash": ubot["api_hash"],
-            "session_string": ubot["session_string"]
-        })
-    return data
 
 async def add_ubot(user_id, api_id, api_hash, session_string):
     await ubotdb.update_one(
