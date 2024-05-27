0f5e7061b21ad1ec9c27a3977d803fbf51a25e51
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-05-27 19:58:07 +0700

    Update expired.py

--- PyroUbot/core/function/expired.py
+++ PyroUbot/core/function/expired.py
@@ -10,10 +10,13 @@ from PyroUbot.config import LOGS_MAKER_UBOT
 from PyroUbot.core.database import *
 from PyroUbot.core.helpers import MSG, Button
 
-
 async def handle_expired_userbot(X):
     try:
         vars = await get_vars(X.me.id, "EXPIRED_DATE")
+        if not vars:
+            await bot.send_message(OWNER_ID, f"Warning: No EXPIRED_DATE for userbot {X.me.id}")
+            return
+        
         time = datetime.now(timezone("Asia/Jakarta")).strftime("%d-%m-%Y")
         exp = vars.strftime("%d-%m-%Y")
         if time == exp:
@@ -34,13 +37,15 @@ async def handle_expired_userbot(X):
                 reply_markup=Button.expired_button_bot(),
             )
             await X.log_out()
+    except pyrogram.errors.exceptions.flood_420.FloodWait as e:
+        await asyncio.sleep(e.x)
+        await bot.send_message(OWNER_ID, f"Flood wait error for userbot {X.me.id}. Retrying...")
+        await handle_expired_userbot(X)
     except Exception as e:
         await bot.send_message(OWNER_ID, f"Error: - {X.me.id}\n{str(e)}")
-
 
 async def expiredUserbots():
     while True:
         get_ubot = [handle_expired_userbot(X) for X in ubot._ubot]
         await asyncio.gather(*get_ubot)
         await asyncio.sleep(10)
-        
