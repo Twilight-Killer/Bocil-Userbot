0705083670cb3584155a5956b9e4fd1d30c81114
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-05-28 19:08:37 +0700

    Update afk.py

--- PyroUbot/modules/afk.py
+++ PyroUbot/modules/afk.py
@@ -13,7 +13,6 @@ __HELP__ = """
   <b>• penjelasan:</b> menonaktifkan afk
 """
 
-
 class AFK:
     def __init__(self, client, message, reason=""):
         self.client = client
@@ -22,11 +21,15 @@ class AFK:
 
     async def set_afk(self):
         db_afk = {"time": time(), "reason": self.reason}
-        msg_afk = (
-            f"<b>❏ sedang afk\n ╰ alasan: {self.reason}</b>"
-            if self.reason
-            else "<b>❏ sedang afk</b>"
-        )
+        if self.client.me.is_premium:
+            msg_afk = (
+                f"<emoji id=5971865795582495562>🔺</emoji> <b>Sedang AFK\n<emoji id=6226230182806554486>🚫</emoji> Alasan: {self.reason}</b>"
+                if self.reason
+                else "<emoji id=5971865795582495562>🔺</emoji> <b>Sedang AFK</b>"
+            )
+        else:
+            msg_afk = f"<b>❏ Sedang AFK\n ╰ Alasan: {self.reason}</b>" if self.reason else "<b>❏ Sedang AFK</b>"
+        
         await set_vars(self.client.me.id, "AFK", db_afk)
         await self.message.reply(msg_afk)
 
@@ -36,11 +39,18 @@ class AFK:
             afk_time = vars.get("time")
             afk_reason = vars.get("reason")
             afk_runtime = await get_time(time() - afk_time)
-            afk_text = (
-                f"<b>❏ sedang afk\n ├ waktu: {afk_runtime}\n ╰ alasan: {afk_reason}</b>"
-                if afk_reason
-                else f"<b>❏ sedang afk\n ╰ waktu: {afk_runtime}</b>"
-            )
+            if self.client.me.is_premium:
+                afk_text = (
+                    f"<emoji id=4927486932113425461>🔺</emoji> <b>Sedang AFK\n<emoji id=5413704112220949842>✅</emoji> Waktu: {afk_runtime}\n<emoji id=5305417940760273444>🚫</emoji> Alasan: {afk_reason}</b>"
+                    if afk_reason
+                    else f"<emoji id=4927486932113425461>🔺</emoji> <b>Sedang AFK\n<emoji id=5413704112220949842>✅</emoji> Waktu: {afk_runtime}</b>"
+                )
+            else:
+                afk_text = (
+                    f"<b>❏ Sedang AFK\n ├ Waktu: {afk_runtime}\n ╰ Alasan: {afk_reason}</b>"
+                    if afk_reason
+                    else f"<b>❏ Sedang AFK\n ╰ Waktu: {afk_runtime}</b>"
+                )
             return await self.message.reply(afk_text)
 
     async def unset_afk(self):
@@ -48,10 +58,12 @@ class AFK:
         if vars:
             afk_time = vars.get("time")
             afk_runtime = await get_time(time() - afk_time)
-            afk_text = f"<b>❏ kembali online\n ╰ afk selama: {afk_runtime}"
+            if self.client.me.is_premium:
+                afk_text = f"<emoji id=5190744409901113661>🌐</emoji> <b>Kembali online\n<emoji id=5413704112220949842>✅</emoji> AFK selama: {afk_runtime}</b>"
+            else:
+                afk_text = f"<b>❏ Kembali online\n ╰ AFK selama: {afk_runtime}</b>"
             await self.message.reply(afk_text)
             return await remove_vars(self.client.me.id, "AFK")
-
 
 @PY.UBOT("afk")
 async def _(client, message):
@@ -59,12 +71,10 @@ async def _(client, message):
     afk_handler = AFK(client, message, reason)
     await afk_handler.set_afk()
 
-
 @PY.AFK()
 async def _(client, message):
     afk_handler = AFK(client, message)
     await afk_handler.get_afk()
-
 
 @PY.UBOT("unafk")
 async def _(client, message):
