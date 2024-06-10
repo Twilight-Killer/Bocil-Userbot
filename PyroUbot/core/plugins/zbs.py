44acc074d42cfe2f3ea2c8f97132498fea4c0471
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-06-10 22:24:39 +0700

    Update zbs.py

--- PyroUbot/core/plugins/zbs.py
+++ PyroUbot/core/plugins/zbs.py
@@ -1,11 +1,25 @@
+from pyrogram.enums import ChatMemberStatus
+
+from PyroUbot import*
+
+
 async def zombies_cmd(client, message):
     chat_id = message.chat.id
+    user_id = message.from_user.id
+    
+    
+    member = await client.get_chat_member(chat_id, user_id)
+    if member.status not in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
+        return await message.reply("Anda harus menjadi admin atau pemilik grup untuk menggunakan perintah ini.")
+    
     deleted_users = []
     banned_users = 0
-    Tm = await message.reply("<code>memeriksa....</code>")
+    Tm = await message.reply("Sedang memeriksa...")
+    
     async for i in client.get_chat_members(chat_id):
         if i.user.is_deleted:
             deleted_users.append(i.user.id)
+    
     if len(deleted_users) > 0:
         for deleted_user in deleted_users:
             try:
@@ -13,6 +27,6 @@ async def zombies_cmd(client, message):
                 await message.chat.ban_member(deleted_user)
             except Exception:
                 pass
-        await Tm.edit(f"<b>berhasil mengeluarkan {banned_users} akun terhapus</b>")
+        await Tm.edit(f"Berhasil mengeluarkan {banned_users} akun terhapus")
     else:
-        await Tm.edit("<b>tidak ada akun terhapus di group ini</b>")
+        await Tm.edit("Tidak ada akun terhapus di grup ini")
