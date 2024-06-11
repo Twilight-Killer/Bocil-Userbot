f81527ef7ddccd72352ecd2c522c93a50094f7a1
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-06-12 00:19:02 +0700

    Update restart.py

--- PyroUbot/modules/restart.py
+++ PyroUbot/modules/restart.py
@@ -6,7 +6,3 @@ from PyroUbot import *
 async def _(client, message):
     await login_cmd(client, message)
 
-
-@PY.BOT("restart")
-async def _(client, message):
-    await restart_cmd(client, message)
