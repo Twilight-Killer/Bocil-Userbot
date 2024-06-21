9659fe4537e4905dccad69d09bd764491f7f77c2
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-06-21 12:47:21 +0700

    Update smd.py

--- PyroUbot/core/plugins/smd.py
+++ PyroUbot/core/plugins/smd.py
@@ -12,16 +12,23 @@ async def sosmed_cmd(client, message):
         bot = "msaver_bot"
         link = message.text.split()[1]
         await client.unblock_user(bot)
-        Tm = await message.reply("<code>prosesss...</code>")
+        Tm = await message.reply("<code>proses...</code>")
         xnxx = await client.send_message(bot, link)
         await asyncio.sleep(10)
         try:
             sosmed = await client.get_messages(bot, xnxx.id + 2)
-            await sosmed.copy(message.chat.id, reply_to_message_id=message.id)
+            media = await sosmed.download()
+            if media:
+                await client.send_message(
+                    chat_id=message.chat.id,
+                    caption=sosmed.caption,
+                    reply_to_message_id=message.id,
+                    file=media
+                )
             await Tm.delete()
-        except Exception:
+        except Exception as e:
             await Tm.edit(
-                "<b>video tidak ditemukan silahksn coba lagi beberapa saat nantik kepeseng</b>"
+                "<b>Video tidak ditemukan. Silakan coba lagi beberapa saat lagi.</b>"
             )
         user_info = await client.resolve_peer(bot)
         return await client.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
