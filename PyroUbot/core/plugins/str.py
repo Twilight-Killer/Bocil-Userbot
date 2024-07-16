6e26def889679ce81a9de86c58d3a398c62cb2e9
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-07-17 04:48:22 +0700

    Update str.py

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -55,9 +55,9 @@ async def ping_cmd(client, message):
 
     if client.me.is_premium:
         _ping = f"""
-<b><emoji id="{emot_pong}">🏓</emoji> Pong:</b></blockquote> <code>{str(delta_ping).replace('.', ',')} ms</code>
-<b><emoji id="{emot_uptime}">⏰</emoji> Uptime:</b></blockquote> <code>{uptime}</code>
-<b><emoji id="{emot_mention}">👑</emoji> Mention:</b></blockquote> <a href="tg://user?id={client.me.id}">{client.me.first_name} {client.me.last_name or ''}</a>
+<b><blockquote><emoji id="{emot_pong}">🏓</emoji> Pong:</b></blockquote> <code>{str(delta_ping).replace('.', ',')} ms</code>
+<b><blockquote><emoji id="{emot_uptime}">⏰</emoji> Uptime:</b></blockquote> <code>{uptime}</code>
+<b><blockquote><emoji id="{emot_mention}">👑</emoji> Mention:</b></blockquote> <a href="tg://user?id={client.me.id}">{client.me.first_name} {client.me.last_name or ''}</a>
         """
     else:
         _ping = f""" 
